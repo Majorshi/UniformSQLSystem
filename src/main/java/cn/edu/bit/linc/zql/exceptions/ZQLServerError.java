091bde940aaaf1c3d2baed1de9a9ba750dbaebaa
ihainan <ihainan72@gmail.com>
2015-09-17 09:56:58 +0800

    Fix: 文件缺失

---
+++ src/main/java/cn/edu/bit/linc/zql/exceptions/ZQLServerError.java
@@ -0,0 +1,42 @@
+package cn.edu.bit.linc.zql.exceptions;
+
+/**
+ * 服务器发生异常
+ */
+public class ZQLServerError extends ZQLException{
+    /**
+     * 构造函数
+     *
+     * @param reason     异常描述
+     * @param SQLState   一个 XOPEN 或者 SQL:2003 代码用于标示异常
+     * @param vendorCode 数据库提供商指定代码用于标示异常
+     */
+    public ZQLServerError(String reason, String SQLState, int vendorCode) {
+        super(reason, SQLState, vendorCode);
+    }
+
+    /**
+     * 构造函数
+     *
+     * @param reason   异常描述
+     * @param SQLState 一个 XOPEN 或者 SQL:2003 代码用于标示异常
+     */
+    public ZQLServerError(String reason, String SQLState) {
+        super(reason, SQLState);
+    }
+
+    /**
+     * 构造函数
+     *
+     * @param reason 异常描述
+     */
+    public ZQLServerError(String reason) {
+        super(reason);
+    }
+
+    /**
+     * 构造函数
+     */
+    public ZQLServerError() {
+    }
+}
